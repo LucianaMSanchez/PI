e47decfb830b44fd05f0fcedf0162d04ae5a2085
LucianaMSanchez <lulutcht@hotmail.com>
2023-08-14 22:19:12 -0300

    PI

--- client/src/Components/Views/About/About.jsx
+++ client/src/Components/Views/About/About.jsx
@@ -17,7 +17,10 @@ const About = () => {
                 type icon, and once you're in you can choose to add any pokemon to your Pokédex.
                 You can also check what pokémons are related to each type of energy and vice versa.
                 Weather you're and expert trainer or you're a newy, you will love this app.</p>
-                <h2 className={style.h2}>We invite you to explore it!!!</h2>
+                <h2 className={style.h2}>We invite you to explore it!!!</h2><br />
+                <p className={style.p}>To PLAY the card GAME: you should go to your Pokédex and choose 4 CHAMPS to compete.
+                Then go to the PLAY page and display 4 random cards. Whenever you're READY click the PLAY button and COMPETE!
+                You can choose differents CHAMPS every time you play. GOOD LUCK!!</p>
             </div>            
             <div>
                 <img className={style.image} src={pokemon} alt="pokemon"/>
@@ -31,7 +34,10 @@ const About = () => {
                 carta o el tipo de ícono correspondiente, y una vez dentro, puedes elegir agregar cualquier pokémon a tu Pokédex.
                 También puedes ver que pokémons estan relacionados con cada tipo de energía y viceversa.
                 Ya seas un entrenador experto o un novato, te va a encantar esta App.</p>
-                <h2 className={style.h2}>Te invitamos a que la explores!!!</h2>  
+                <h2 className={style.h2}>Te invitamos a que la explores!!!</h2><br />
+                <p className={style.p}>Para JUGAR el JUEGO de cartas: debes dirigirte a tu Pokédex y elegir 4 CHAMPS para competir.
+                Luego dirígete a la pagina PLAY y despliega 4 cartas random. Cuando estés listo clickea en el botón PLAY y COMPITE!
+                Puedes elegir diferentes CHAMPS cada vez que juegas. BUENA SUERTE!!</p>
             </div>
 
         </div>
